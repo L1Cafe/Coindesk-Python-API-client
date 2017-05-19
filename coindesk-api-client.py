4ad6065db48a16ffbb0e3313e8a1cb8297db4fa9
L-Cafe <L-Cafe@linux.com>
2017-05-19 23:24:52 +0200

    PEP

--- coindesk-api-client.py
+++ coindesk-api-client.py
@@ -1,25 +1,28 @@
 import requests
-from datetime import date,timedelta
+from datetime import date, timedelta
 
 coindeskApi = "http://api.coindesk.com/v1/bpi/"
 
+
 def get_currencies():
-    coindeskCurrencies = requests.get(coindeskApi + "supported-currencies.json").json()
-    currencyList = []
-    for dict in coindeskCurrencies:
-        currencyList.append(dict["currency"])
-    return currencyList
+    coindesk_currencies = requests.get(coindeskApi + "supported-currencies.json").json()
+    currency_list = []
+    for currency in coindesk_currencies:
+        currency_list.append(currency["currency"])
+    return currency_list
+
 
 def get_current_price(currency):
-    return float(requests.get(coindeskApi + "currentprice/" + currency + ".json").json()["bpi"][currency]["rate"] \
-        .replace(",", ""))
+    return float(requests.get(coindeskApi + "currentprice/" + currency + ".json").json()["bpi"][currency]["rate"]
+                 .replace(",", ""))
+
 
 def get_historical_price(
-        currency
-        , start=date.today()-timedelta(days=31)
-        , end=date.today()
+        currency,
+        start=date.today()-timedelta(days=31),
+        end=date.today()
         ):
-    startString = "%02d" % start.year + "-" + "%02d" % start.month + "-" + "%02d" % start.day
-    endString = "%02d" % end.year + "-" + "%02d" % end.month + "-" + "%02d" % end.day
-    return requests.get(coindeskApi + "historical/close.json?currency=" + currency + "&start=" + startString +  "&end=" + endString).json()["bpi"]
-
+    start_string = "%02d" % start.year + "-" + "%02d" % start.month + "-" + "%02d" % start.day
+    end_string = "%02d" % end.year + "-" + "%02d" % end.month + "-" + "%02d" % end.day
+    return requests.get(coindeskApi + "historical/close.json?currency="
+                        + currency + "&start=" + start_string + "&end=" + end_string).json()["bpi"]
