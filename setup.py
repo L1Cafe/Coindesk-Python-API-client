6cb47a54cac728238e09b33fa891a244276f0310
L-Cafe <L-Cafe@linux.com>
2017-05-20 02:25:20 +0200

    test

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='Coindesk Python API client',
-    version='0.0.1',
+    version='0.0.1a1',
     description='This library fetches Coindesk\'s Bitcoin Price Index from their API.',
     long_description=long_description,
     url='https://github.com/L-Cafe/Coindesk-Python-API-client',
@@ -27,10 +27,10 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     keywords='bitcoin coindesk bpi price',
-
+    install_requires=['requests']
     # TODO You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     # TODO Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -40,17 +40,17 @@ setup(
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
+    #extras_require={
+    #    'dev': ['check-manifest'],
+    #    'test': ['coverage'],
+    #},
 
     # TODO To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    },
+    #entry_points={
+    #    'console_scripts': [
+    #        'sample=sample:main',
+    #    ],
+    #},
 )
